20581d4bd86526a2730d59be6aff7e511a414389
jabarros <jabarros@solidgear.es>
2016-06-01 13:55:53 +0200

    Create method to check if file is favorite or belongs to a favorite folder, and change favorite attribute from boolean to emum to take into account the 3 cases (favorite, no-favorite and parent folder favorite)

--- src/com/owncloud/android/operations/RefreshFolderOperation.java
+++ src/com/owncloud/android/operations/RefreshFolderOperation.java
@@ -412,7 +412,7 @@ public class RefreshFolderOperation extends RemoteOperation {
             FileStorageUtils.searchForLocalFileInDefaultPath(updatedFile, mAccount);
 
             /// prepare content synchronization for kept-in-sync files
-            if (updatedFile.isFavorite() ) {
+            if (updatedFile.isFavorite() == OCFile.FavoriteStatus.FAVORITE.getValue()) {
                 if (updatedFile.isFolder()) {
                     SynchronizeFolderOperation operation = new SynchronizeFolderOperation(
                             mContext,
@@ -532,7 +532,7 @@ public class RefreshFolderOperation extends RemoteOperation {
     private void fetchFavoritesToSyncFromLocalData() {
         List<OCFile> children = mStorageManager.getFolderContent(mLocalFolder);
         for (OCFile child : children) {
-            if (!child.isFolder() && child.isFavorite()) {
+            if (!child.isFolder() && child.isFavorite() == OCFile.FavoriteStatus.FAVORITE.getValue()) {
                 SynchronizeFileOperation operation = new SynchronizeFileOperation(
                         child,
                         child,  // cheating with the remote file to get an update to server; to refactor
